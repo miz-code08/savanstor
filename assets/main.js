80f9ea2346f25f6074e4289bfeb99f8c0af45aee
miz <buiductai357@gmail.com>
2024-10-16 15:46:42 +0700

    member

--- assets/main.js
+++ assets/main.js
@@ -7,62 +7,108 @@ window.onload = function() {
     const menu = document.querySelector(`.menu`);
     const menuBtn = document.querySelector(`.fa-bars`);
     const menuBtnClose = document.querySelector(`.fa-xmark`);
+    const menuLinks = document.querySelectorAll(`.menu-link`);
+    let menuCntSlide = 0;
+    let menuTimeSlide = 10000;
 
-    let cntSlide = 0;
-    let timeSlide = 10000;
+    const memberLinks = document.querySelectorAll(`.member-link`);
+    const memberBtnLeft = document.querySelector(`.member__btn--left`);
+    const memberBtnRight = document.querySelector(`.member__btn--right`);
+    let memberCnt = 1;
 
-
-    // thêm active thanh navbar
+    // under thanh navbar
     navLink.forEach((val, idx) => {
-        val.addEventListener("mouseenter", function() {
+        val.addEventListener("mouseenter", () => {
             underLine[idx].style.opacity = "1";            
         });
-        val.addEventListener("mouseleave", function() {
+        val.addEventListener("mouseleave", () => {
             underLine[idx].style.opacity = "0";
         });
     });
 
+    // thêm class panelActive vào slide ảnh home
     panel.forEach((val, idx) => {
-        val.addEventListener("click", function() {
-            removeActive();
-            val.classList.add("active");
-            cntSlide = idx;
+        val.addEventListener("click", () => {
+            removePanelActive();
+            val.classList.add("panelActive");
+            menuCntSlide = idx;
         });
     });
 
-    function removeActive() {
+    function removePanelActive() {
         panel.forEach(val => {
-            val.classList.remove("active");
+            val.classList.remove("panelActive");
         }) 
     }
 
     // chạy slide ảnh home
     if (window.innerWidth >= 768) 
-        setInterval(slidePicture, timeSlide)    
+        setInterval(slidePicture, menuTimeSlide)    
 
     function slidePicture() {
         panel.forEach(val => val.addEventListener("click", () => {
             return;
         }));
-        cntSlide++;
-        if(cntSlide > panel.length - 1)
-            cntSlide = 0;
-        removeActive();
-        panel[cntSlide].classList.add("active");
+        menuCntSlide++;
+        if(menuCntSlide > panel.length - 1)
+            menuCntSlide = 0;
+        removePanelActive();
+        panel[menuCntSlide].classList.add("panelActive");
     }
 
     // mở đóng menu
-    menuBtn.addEventListener("click", function() {
+    menuBtn.addEventListener("click", () => {
         menu.style.opacity = "1";
         menu.style.visibility = "visible";
         menuBtn.style.display = "none";
         menuBtnClose.style.display = "block";
     });
 
-    menuBtnClose.addEventListener("click", function() {
+    menuBtnClose.addEventListener("click", () => {
         menu.style.opacity = "0";
         menu.style.visibility = "hidden";
         menuBtn.style.display = "block";
         menuBtnClose.style.display = "none";
     });
+
+    menuLinks.forEach(val => {
+        val.addEventListener("click", () => {
+            menu.style.opacity = "0";
+            menu.style.visibility = "hidden";
+            menuBtn.style.display = "block";
+            menuBtnClose.style.display = "none";
+        })
+    })
+
+    // chạy member slide bằng nút bấm
+    memberBtnRight.addEventListener("click", () => {
+        if(memberCnt + 2 < memberLinks.length) {
+            memberCnt++;
+            memberTranslate();
+        }
+        else if (window.innerWidth <= 767.98) {
+            if(memberCnt < memberLinks.length) {
+                memberCnt++;
+                memberTranslate();
+            }
+        }
+    });
+
+    memberBtnLeft.addEventListener("click", () => {
+        if(memberCnt - 1 > 0) {
+            memberCnt--;
+            memberTranslate();
+        }
+    });
+
+    function memberTranslate() {
+        memberLinks.forEach(val => {
+            let translate = (memberCnt - 1) * 100;
+            console.log(translate, 20 * (memberCnt - 1));
+            val.style.translate = `calc(-${translate}% - ${20 * (memberCnt - 1)}px)`;
+        });
+    }
+
+
+    
 };
